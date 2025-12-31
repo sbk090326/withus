9c691402795365f7be2ef797be0ec7f794eddf07
sbk090326 <sbk090326@gmail.com>
2025-12-31 13:37:47 +0900

    Initial commit: Withus project structure

---
+++ withus-backend/src/main/java/com/withus/withus_backend/WithusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.withus.withus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithusBackendApplication.class, args);
+	}
+
+}
---
+++ withus-backend/src/test/java/com/withus/withus_backend/WithusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withus.withus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
